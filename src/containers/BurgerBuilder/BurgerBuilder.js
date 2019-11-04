88041d630f491a8054a79f4472285c07d2bff9f3
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2019-11-04 12:24:01 +0530

    final commit

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
+
 import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <div>Burger</div>
-        <div>BuildControl</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Aux>
     );
   }
